2853aa6a1ef163175b681fc4ab3865e656101ba5
Lambda <lambda@smail.kongju.ac.kr>
2024-12-17 15:27:02 +0900

    주석 달기

--- Snake_Game/snake_main.c
+++ Snake_Game/snake_main.c
@@ -9,23 +9,25 @@
 #include <string.h>
 
 
-struct BODY
+/// 구조체 선언 ///
+
+typedef struct BODY
 {
 	int x, y, direction; //direction : 1 -> -y, 2 -> -x, 3 -> +y, 4-> +x
 	int body_no;
 	struct BODY* next;
-};
-typedef struct BODY body; // 타입 정의
+} body; // 타입 정의
 
 
-struct BOMB_BOX
+typedef struct BOMB_BOX
 {
 	int x, y;
-};
-typedef struct BOMB_BOX BOMB;
+}  BOMB;
+
+/// 구조체 선언 종료 ///
 
 
-
+/// enum 선언 ///
 
 enum Speed {
 	lvl1 = 300,
@@ -55,7 +57,11 @@ enum ColorType {
 	White		//15
 };
 
+/// enum 선언 동료 ///
 
+
+
+/// 전역 변수 선언 ///
 
 body * head = NULL; // 머리 선언
 
@@ -83,34 +89,25 @@ char player[20] = "Anonym"; // 플레이어 이름, 기본값 = "Anonym"
 char best_player[20]; // 최고 점수 기록자 이름
 int highest_score = 0; // 최고 점수
 
+/// 전역 변수 선언 종료 ///
 
 
+/// 함수 선언 ///
 
-
-
-
-
-// 레벨 선택
+// 레벨 선택, 처음 인트로 부분
 int select_level();
-
 
 // 대화 상자 그리기
 void draw_dlg_box(int box_width, int box_height, int start_x);
 
-
-// 입력 예시(힌트) 설정
+// 입력 예시(힌트) 설정, 입력 받은 좌표에 입력받은 힌트 메시지 표시
 void set_hint(int x, int y, char hint[30]);
-
 
 // 엔터 입력을 기다리는 함수
 void wait_enter();
 
-// 지정한 좌표로 커서를 이동해 " "를 출력하여 보이는 글자를 지우는 함수
+// 지정한 좌표로 커서를 이동해 " "(공백)를(을) 출력하여 보이는 글자를 지우는 함수
 void delete_line(int x, int y);
-
-
-
-
 
 // 몸통 생성, 머리 혹은 앞 몸통의 좌표, 보고 있는 방향을 받아 그 다음 위치에 좌표 설정, 바라보는 방향 설정
 body* create_body(int x, int y, int direction, int body_no);
@@ -124,37 +121,19 @@ void insert_body(body* st, body* new_body);
 // 좌표를 받아 해당좌표에 몸통이 있는지 확인하는 함수
 int round_snake(body* st, int x, int y);
 
-
-
-
-
-
 // 머리를 제외한 몸통을 움직이는 기능
 // 앞 몸통의 좌표 저장(prev_x, prev_y), 자신의 좌표 임시 저장(temp_x, temp_y), 자신에게 앞 몸통 좌표 저장, 자신의 좌표 였던 좌표 prev_x, prevy애 저장
 void move_body();
 
-
-
-
-
-
-
-// 1 -> 미스테리 박스 점수 변동 기능에 사용, 매개 변수로 받은 change 값을 더 함(change가 음수인 경우에 점수가 0 이하이면 점수 변화 X (2024.11.23 수정)
+// 1 -> 미스테리 박스 점수 변동 기능에 사용, 매개 변수로 받은 change 값을 더 함(change가 음수인 경우에 점수가 0 이하이면 점수 변화 X
 // 2 -> 폭탄 박스를 위한 기능, 현재 점수와는 상관 없이 무조건 점수를 변화 시킴
 void change_score(int num, int change);
-
 
 // 1~r 사이의 랜덤한 값을 반환
 int random_num(int r);
 
-
-
 //  1~5 사이 개수의 몸통 추가
 void body_add(int r);
-
-
-
-
 
 // 커서 위치 이동
 void gotoxy(int x, int y);
@@ -165,13 +144,7 @@ void textcolor(enum ColorType color);
 // 한영키 누르기
 int KEYINPUT();
 
-
-
-
-
-
-// Function to generate the fruit 
-// within the boundary 
+// 기본적인 시스템적인 요소를 설정하고 게임 요소를 만드는 함수 
 void setup();
 
 // Function to draw the boundaries 
@@ -184,16 +157,11 @@ void input();
 // each movement 
 void logic();
 
-
+// 게임 종료시 화면, 기록을 보여주며 하이 스코어가 바뀌는지 알려준다.
 void gameover_screen();
-
-
-
 
 // 동적 할당된 모든 body(node) 동적 할당 해제
 void free_snake(body* node);
-
-
 
 // record.txt 파일에서 기록을 읽어와 최고 점수와 플레이어를 화면에 띄울 수 있도록 하는 함수
 int read_record();
@@ -202,6 +170,7 @@ int read_record();
 int write_record();
 
 
+/// 함수 선언 종료 ///
 
 
 
@@ -230,11 +199,13 @@ void main()
 }
 
 
+/// 함수 정의 ///
 
+/* 대화 파트 */
 
 // 레벨 선택, 처음 인트로 부분
 int select_level() {
-	enum Speed speed = lvl1;
+	enum Speed speed = lvl1; // 지역 변수 speed
 	int sel = 0;
 	char answer[20];
 
@@ -273,7 +244,7 @@ int select_level() {
 	delete_line(9, 11);
 
 
-
+	// 게임 방법 설명
 	if (!strcmp(s, "모르지렁")) {
 		gotoxy(15, 4);
 		printf("설명 시작하겠지렁                ");
@@ -360,7 +331,7 @@ int select_level() {
 	}
 
 
-
+	// 게임 플에이 여부 질문
 	gotoxy(15, 4);
 	printf("내가 성장할 수 있도록 잘 도와줄 수 있지렁?");
 
@@ -371,16 +342,16 @@ int select_level() {
 	gotoxy(9, 11);
 	scanf_s("%s", answer, (int)sizeof("당연하지렁"));
 
-	gotoxy(9, 11);
-	printf("                        ");
+	
+	delete_line(9, 11);
 
 
-
+	// "당연하지렁" -> 게임 진행
+	// (다른 입력) -> 종료
 	if (strcmp(answer, "당연하지렁") == 0) {
-		gotoxy(15, 4);
-		printf("                                                        ");
-		gotoxy(15, 5);
-		printf("                                                        ");
+		delete_line(15, 4);
+		delete_line(15, 5);
+
 
 		gotoxy(15, 4);
 		printf("좋았지렁!! 그럼 이제 이름을 말해지렁!        ");
@@ -404,7 +375,7 @@ int select_level() {
 		printf("Level ");
 		scanf_s("%d", &sel);
 
-		KEYINPUT();
+		KEYINPUT(); // 한영키 입력
 	}
 	else {
 		gotoxy(15, 4);
@@ -455,6 +426,7 @@ int select_level() {
 // 대화 상자 그리기
 void draw_dlg_box(int box_width, int box_height, int start_x) {
 	
+	// 입력 받은 크기 만큼 박스를 그린다.
 	for (int i = 0; i < box_height; i++) {
 		gotoxy(start_x, i);
 		for (int j = 0; j < box_width; j++) {
@@ -474,7 +446,7 @@ void draw_dlg_box(int box_width, int box_height, int start_x) {
 }
 
 
-// 입력 예시(힌트) 설정
+// 입력 예시(힌트) 설정, 입력 받은 좌표에 입력받은 힌트 메시지 표시
 void set_hint(int x, int y, char hint[30]) {
 	gotoxy(x, y);
 	textcolor(DarkGray);
@@ -488,7 +460,8 @@ void wait_enter() {
 	while (!(_kbhit() && getchar() == '\n')) {}
 }
 
-// 지정한 좌표로 커서를 이동해 " "를 출력하여 보이는 글자를 지우는 함수
+
+// 지정한 좌표로 커서를 이동해 " "(공백)를(을) 출력하여 보이는 글자를 지우는 함수
 void delete_line(int x, int y) {
 	gotoxy(x, y);
 	printf("                                                               ");
@@ -497,6 +470,7 @@ void delete_line(int x, int y) {
 
 
 
+/* 지렁이 몸통 노드 관리 파트 */
 
 // 몸통 생성, 머리 혹은 앞 몸통의 좌표, 보고 있는 방향을 받아 그 다음 위치에 좌표 설정, 바라보는 방향 설정
 body* create_body(int x, int y, int direction, int body_no) {
@@ -529,6 +503,7 @@ body* create_body(int x, int y, int direction, int body_no) {
 	return new_body;
 }
 
+
 // 마지막 몸통(꼬리) 탐색
 body* find_tail(body* st) {
 	body* cur = st;
@@ -539,11 +514,13 @@ body* find_tail(body* st) {
 	return cur;
 }
 
+
 // 생성한 몸통을 가장 마지막에 연결
 void insert_body(body* st, body* new_body) {
 	body* tail = find_tail(st);
 	tail->next = new_body;
 }
+
 
 // 좌표를 받아 해당좌표에 몸통이 있는지 확인하는 함수
 int round_snake(body* st, int x, int y) {
@@ -560,10 +537,6 @@ int round_snake(body* st, int x, int y) {
 
 	return 0;
 }
-
-
-
-
 
 
 // 머리를 제외한 몸통을 움직이는 기능
@@ -595,11 +568,22 @@ void move_body() {
 }
 
 
+// 동적 할당된 모든 body(node) 동적 할당 해제
+void free_snake(body* node) {
+	body* cur = node;
+	body* next;
+
+	while (cur->next != NULL) {
+		next = cur->next;
+		free(cur);
+		cur = next;
+	}
+}
 
 
 
 
-
+/* 이벤트 관리 */
 
 // 1 -> 미스테리 박스 점수 변동 기능에 사용, 매개 변수로 받은 change 값을 더 함(change가 음수인 경우에 점수가 0 이하이면 점수 변화 X (2024.11.23 수정)
 // 2 -> 폭탄 박스를 위한 기능, 현재 점수와는 상관 없이 무조건 점수를 변화 시킴
@@ -607,7 +591,7 @@ void change_score(int num, int change) {
 
 	switch (num) {
 	case 1:
-		score += (score > 0 || change > 0) ? change : 0;
+		score += ((score + change) >= 0 || change > 0) ? change : 0;
 		break;
 
 	case 2:
@@ -628,7 +612,6 @@ int random_num(int r) {
 }
 
 
-
 //  1~5 사이 개수의 몸통 추가
 void body_add(int r) {
 	for (int i = 0; i < r; i++) {
@@ -642,7 +625,7 @@ void body_add(int r) {
 
 
 
-
+/* 콘솔 및 하드웨어 컨트롤 파트 */
 
 // 커서 위치 이동
 void gotoxy(int x, int y) {
@@ -670,10 +653,14 @@ int KEYINPUT() {// 한영키 누름
 }
 
 
-// Function to generate the fruit 
-// within the boundary 
+
+
+/* 게임 시스템 파트 */
+
+// 기본적인 시스템적인 요소를 설정하고 게임 요소를 만드는 함수 
 void setup()
 {
+	// 콘솔 크기 설정
 	system("mode con:cols=150 lines=40");
 
 	// 난이도 선택
@@ -683,7 +670,7 @@ void setup()
 
 
 	// 커서 세팅
-	CONSOLE_CURSOR_INFO cursorInfo = { 0, }; // 찾아봐야 함
+	CONSOLE_CURSOR_INFO cursorInfo = { 0, }; // CONSOLE_CURSOR_INFO 구조체를 선언ㅇ
 	cursorInfo.dwSize = 1; // 커서 굵기 (1 ~ 100)
 	cursorInfo.bVisible = FALSE; // 커서 Visible TRUE(보임) FALSE(숨김)
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo); // 설정 적용
@@ -692,14 +679,14 @@ void setup()
 	textcolor(White); // 글자 색깔 변환 (기본 : Gray)
 
 
-	read_record();
+	read_record(); // 최고 기록 읽어와 최고 플레이어, 최고 점수 변수에 저장
 
 	gameover = 0;
 
 	head = (body*)malloc(sizeof(body));
 
 	// Stores height and width 
-	// head 내 변수 초기화
+	// head의 구조체 변수 초기화
 	head->x = height / 2;
 	head->y = width / 2;
 	head->next = NULL;
@@ -709,7 +696,7 @@ void setup()
 
 	cut_bodies = (body*)malloc(sizeof(body));
 
-	// cut_bodies 내 변수 초기화
+	// cut_bodies의 구조체 변수 초기화
 	cut_bodies->x = NULL;
 	cut_bodies->y = NULL;
 	cut_bodies->next = NULL;
@@ -717,7 +704,7 @@ void setup()
 	cut_bodies->body_no = NULL;
 
 
-	// fruit
+	// fruit 위치 설정
 	fruitx = 0;
 	fruity = 0;
 
@@ -731,7 +718,7 @@ void setup()
 
 
 
-	// event
+	// event 위치 설정
 	mysteryx = 0;
 	mysteryy = 0;
 
@@ -745,7 +732,7 @@ void setup()
 
 
 
-	// trap
+	// trap 위치 설정
 	trapx = 0;
 	trapy = 0;
 
@@ -761,8 +748,9 @@ void setup()
 
 
 
-	// bomb
-	for (int i = 0; i < 4; i++) {
+	// bomb 위치 설정
+
+	for (int i = 0; i < 4; i++) { // bomb초기화
 		bomb[i].x = NULL;
 		bomb[i].y = NULL;
 	}
@@ -784,16 +772,18 @@ void setup()
 	score = 0;
 }
 
+
 // Function to draw the boundaries 
 void draw()
 {
-	gotoxy(0, 0); // 커서 위치 처음으로 보내기 -> 지우고 다시 작성하므로써 발생한 깜빡임 제거
+	gotoxy(0, 0); // 커서 위치 처음으로 보내기 -> 지우고 다시 작성하므로써 발생했던 깜빡임 제거 (덮어씌우므로써 해결)
 	//system("cls");
 
 	for (i = 0; i < height; i++) {
-		gotoxy(29, i);
+		gotoxy(29, i); // 특정 x위치로 이동 높이(y)는 반복문에 따라 변경) 줄 바꿈을 위함
 		for (j = 0; j < width; j++) {
 			
+			// 테두리
 			if (i == 0 && j == 0) printf("┏");
 			else if (i == 0 && j == width - 1) printf("┓");
 			else if (i == height - 1 && j == 0) printf("┗");
@@ -801,6 +791,7 @@ void draw()
 			else if (j == 0 || j == width - 1) {
 				printf("┃");
 			
+				// 게임 정보 표시
 				if (i == 1 && j == width - 1) printf(" Best player\t: %s\t ", best_player);
 				if (i == 2 && j == width - 1) printf(" Highest Score\t: %d\t ", highest_score);
 
@@ -809,37 +800,37 @@ void draw()
 				if (i == 6 && j == width - 1) printf(" Level\t\t: %d\t ", level);
 
 
-				//if (i == 20 && j == width - 1) printf(" press X to quit the game\n");
 			}
-			else if (i == 0 || i == height - 1) printf("━");
-			else {
+			else if (i == 0 || i == height - 1) printf("━"); // 테두리 끝
+			else { // 요소 그리기 head, 몸통, cut_bodies, fruit, trap, mystery, bomb
+				// head
 				if (i == head->x && j == head->y) {
 					printf("O");
 				}
+				// 몸통, cut_bodies
 				else if (round_snake(head, i, j) == 1 || round_snake(cut_bodies, i, j) == 1) // 영역을 그리고 있는 위치에 몸통이 있는지 확인 -> 존재 O => 'o'출력
 				{
 					printf("o");
 				}
+				// fruit
 				else if (i == fruitx && j == fruity)
 				{
 					printf("*");
 
 				}
-				else if (i == bomb[0].x && j == bomb[0].y
-					|| i == bomb[1].x && j == bomb[1].y
-					|| i == bomb[2].x && j == bomb[2].y
-					|| i == bomb[3].x && j == bomb[3].y) // bomb의 모든 좌표 확인
+				// bomb
+				else if ((i == bomb[0].x && j == bomb[0].y)
+					|| (i == bomb[1].x && j == bomb[1].y)
+					|| (i == bomb[2].x && j == bomb[2].y)
+					|| (i == bomb[3].x && j == bomb[3].y)) // bomb의 모든 좌표 확인
 				{
 					printf("▣");
 
 				}
+				// mystery
 				else if (i == mysteryx && j == mysteryy)
 				{
 					if (mystery_cnt == 1) {
-						printf("?");
-					}
-					else if (rand() % 100 < 10 && mystery_cnt == 0) {
-						mystery_cnt = 1;
 						printf("?");
 					}
 					else
@@ -847,11 +838,13 @@ void draw()
 						printf(" ");
 					}
 				}
+				//trap
 				else if (i == trapx && j == trapy)
 				{
-					printf("◎");//기호 바꿔도 댐
+					printf("◎");
 
 				}
+				//공백
 				else
 				{
 					printf(" ");
@@ -862,15 +855,13 @@ void draw()
 		}
 		printf("\n");
 	}
-
-	// Print the score after the 
-	// game ends 
 	
 
 	gotoxy(30, height);
 	printf("press X to quit the game\n");
 	
 }
+
 
 // Function to take the input 
 void input()
@@ -901,31 +892,29 @@ void input()
 	}
 }
 
+
 // Function for the logic behind 
 // each movement 
 void logic()
 {
 	// 점수가 50이 증가할 때마다 레벨 +1
-	if (score > 50) {
-		if (st_level == 1) level = 2;
-		if (st_level == 2) level = 3;
-		if (st_level == 3) level = 4;
-		if (st_level == 4) level = 5;
+	if (score > 200) {
+		if (st_level == 1) level = 5;
 	}
-
-	if (score > 100) {
+	else if (score > 150) {
+		if (st_level == 1) level = 4;
+		if (st_level == 2) level = 5;
+	}
+	else if (score > 100) {
 		if (st_level == 1) level = 3;
 		if (st_level == 2) level = 4;
 		if (st_level == 3) level = 5;
 	}
-
-	if (score > 150) {
-		if (st_level == 1) level = 4;
-		if (st_level == 2) level = 5;
-	}
-
-	if (score > 200) {
-		if (st_level == 1) level = 5;
+	else if (score > 50) {
+		if (st_level == 1) level = 2;
+		if (st_level == 2) level = 3;
+		if (st_level == 3) level = 4;
+		if (st_level == 4) level = 5;
 	}
 
 
@@ -951,7 +940,7 @@ void logic()
 			while (bomb[1].x == 0 || bomb[1].y == 0
 				|| (bomb[1].x == fruitx && bomb[1].y == fruity)
 				|| (bomb[1].x == mysteryx && bomb[1].y == mysteryy)
-				|| (bomb[1].x == fruitx && bomb[1].y == fruity)
+				|| (bomb[1].x == trapx && bomb[1].y == trapy)
 				|| (bomb[1].x == bomb[0].x && bomb[1].y == bomb[0].y)
 				|| (round_snake(head, bomb[1].x, bomb[1].y))) 
 			{
@@ -977,7 +966,7 @@ void logic()
 			while (bomb[2].x == 0 || bomb[2].y == 0
 				|| (bomb[2].x == fruitx && bomb[2].y == fruity)
 				|| (bomb[2].x == mysteryx && bomb[2].y == mysteryy)
-				|| (bomb[2].x == fruitx && bomb[2].y == fruity)
+				|| (bomb[2].x == trapx && bomb[2].y == trapy)
 				|| (bomb[2].x == bomb[0].x && bomb[2].y == bomb[0].y)
 				|| (bomb[2].x == bomb[1].x && bomb[2].y == bomb[1].y)
 				|| (round_snake(head, bomb[2].x, bomb[2].y))) 
@@ -994,7 +983,7 @@ void logic()
 			while (bomb[3].x == 0 || bomb[3].y == 0
 				|| (bomb[3].x == fruitx && bomb[3].y == fruity)
 				|| (bomb[3].x == mysteryx && bomb[3].y == mysteryy)
-				|| (bomb[3].x == fruitx && bomb[3].y == fruity)
+				|| (bomb[3].x == trapx && bomb[3].y == trapy)
 				|| (bomb[3].x == bomb[0].x && bomb[3].y == bomb[0].y)
 				|| (bomb[3].x == bomb[1].x && bomb[3].y == bomb[1].y)
 				|| (bomb[3].x == bomb[2].x && bomb[3].y == bomb[2].y)
@@ -1104,23 +1093,29 @@ void logic()
 
 	// snake가 event(mystery)에 닿았을 때
 	if ((head->x == mysteryx && head->y == mysteryy) /*|| (round_snake(eventx, eventy) == 1)*/) {
+		mystery_cnt--;
 
 		// event(mystery)
 		mysteryx = 0;
 		mysteryy = 0;
 
-		while (mysteryx == 0 || mysteryy == 0
-			|| (mysteryx == fruitx && mysteryy == fruity)
-			|| (mysteryx == trapx && mysteryy == trapy)
-			|| (mysteryx == bomb[0].x && mysteryy == bomb[0].y)
-			|| (mysteryx == bomb[1].x && mysteryy == bomb[1].y)
-			|| (mysteryx == bomb[2].x && mysteryy == bomb[2].y)
-			|| (mysteryx == bomb[3].x && mysteryy == bomb[3].y)
-			|| (round_snake(head, mysteryx, mysteryy))) 
-		{
-			mysteryx = rand() % (height - 1);
-			mysteryy = rand() % (width - 1);
+		if (rand() % 100 < 10) { // mystery 생성은 10% 확률
+			while (mysteryx == 0 || mysteryy == 0
+				|| (mysteryx == fruitx && mysteryy == fruity)
+				|| (mysteryx == trapx && mysteryy == trapy)
+				|| (mysteryx == bomb[0].x && mysteryy == bomb[0].y)
+				|| (mysteryx == bomb[1].x && mysteryy == bomb[1].y)
+				|| (mysteryx == bomb[2].x && mysteryy == bomb[2].y)
+				|| (mysteryx == bomb[3].x && mysteryy == bomb[3].y)
+				|| (round_snake(head, mysteryx, mysteryy)))
+			{
+				mysteryx = rand() % (height - 1);
+				mysteryy = rand() % (width - 1);
+			}
+
+			mystery_cnt = 1;
 		}
+			
 
 		int effect = rand() % 3;
 		switch (effect) {
@@ -1138,7 +1133,6 @@ void logic()
 		}
 
 
-		mystery_cnt--;
 
 	}
 
@@ -1187,13 +1181,14 @@ void logic()
 
 
 
-	// snake가 끊어진 몸통에 닿았을 때
+	// snake가 끊어진 몸통에 닿았을 때, 게임 종료
 	if (round_snake(cut_bodies, head->x, head->y) == 1) {
 		gameover = 1;
 	}
 }
 
 
+// 게임 종료시 화면, 기록을 보여주며 하이 스코어가 바뀌는지 알려준다.
 void gameover_screen() {
 	system("cls");
 
@@ -1235,24 +1230,7 @@ void gameover_screen() {
 }
 
 
-
-
-
-// 동적 할당된 모든 body(node) 동적 할당 해제
-void free_snake(body* node) {
-	body* cur = node;
-	body* next;
-
-	while (cur->next != NULL) {
-		next = cur->next;
-		free(cur);
-		cur = next;
-	}
-}
-
-
-
-
+// record.txt 파일에서 기록을 읽어와 최고 점수와 플레이어를 화면에 띄울 수 있도록 하는 함수
 int read_record() {
 	FILE* fp = NULL;
 
@@ -1266,6 +1244,7 @@ int read_record() {
 }
 
 
+// 게임 종료 시점에 현재 플레이어의 점수와 최고 점수를 비교하여 최고 점수를 갱신 혹은 유지하는 함수, record.txt 파일에 점수를 저장한다.
 int write_record() {
 	if (score > highest_score) {
 		FILE* fp = NULL;
@@ -1278,3 +1257,5 @@ int write_record() {
 		fclose(fp);
 	}
 }
+
+/// 함수 정의 종료 ///
